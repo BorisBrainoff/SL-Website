90a01da4f4bb1204fd2c5793b894a3352212b960
Boris Brainoff <borisbrainoff@boriss-mbp.mynetworksettings.com>
2025-06-25 12:49:51 -0400

    Fix Vite import path error

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button } from "/components/ui/button";
+import { Button } from "../components/ui/button";
+
 import "@fontsource/cormorant-garamond";
 
 export default function HomePage() {
